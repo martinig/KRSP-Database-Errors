d0b2c4b4d14945da07e1bc2c383c95e8a7121545
David N. Fisher <davidnfisher@hotmail.com>
2018-01-19 15:47:12 -0500

    Add files via upload
    R code to create two different pedigrees, one based on the "pedigree" table, the other on the "flastall" table. The latter is nearly twice as large.
    
    Also code to detect mothers/fathers present as dams/sires but not in their own right. These only occur in the flastall based pedigree, 48 dams and 1 sire.

---
+++ Different size pedigrees and missing mums.R
@@ -0,0 +1,48 @@
+require(krsp)
+require(tidyverse)
+
+con = krsp_connect(group = "krsp-aws")
+
+ped=tbl(con, "pedigree") %>% collect() %>%
+  select(animal=squirrel_id, sire=sire_id, dam=dam_id) %>%
+  filter(!(is.na(animal)))
+
+pedf=tbl(con, "flastall") %>% collect() %>%
+  select(animal=squirrel_id, sire=sire_id, dam=dam_id) %>%
+  filter(!(is.na(animal)))
+
+nrow(ped)
+nrow(pedf) #so this one is twice as large
+
+#Yet its not down to duplicated entries:
+pedf  %>%  count(animal) %>% filter(n > 1)
+
+ped  %>% count(animal) %>% filter(n > 1)
+
+#Identifying missing mums
+
+miss.mums.ped = ped %>% filter(!is.na(dam), !(dam %in% animal))  %>%
+                                  select(animal=dam) %>% mutate(sire=NA, dam=NA) %>%
+                                  group_by(animal) %>% sample_n(1)
+#None, so that is good
+
+miss.mums.flall = pedf %>% filter(!is.na(dam), !(dam %in% animal))  %>%
+                                  select(animal=dam) %>% mutate(sire=NA, dam=NA) %>%
+                                  group_by(animal) %>% sample_n(1)
+#48 here
+
+#Do same for dads
+miss.dads.ped = ped %>% filter(!is.na(sire), !(sire %in% animal))  %>%
+  select(animal=sire) %>% mutate(sire=NA, dam=NA) %>%
+  group_by(animal) %>% sample_n(1)
+#None, so that is good
+
+miss.dads.flall = pedf %>% filter(!is.na(sire), !(sire %in% animal))  %>%
+  select(animal=sire) %>% mutate(sire=NA, dam=NA) %>%
+  group_by(animal) %>% sample_n(1)
+#1 individual
+
+
+#These could be row bound to the pedigree:
+
+pedf.all = bind_rows(pedf, miss.mums.flall, miss.dads.flall)
